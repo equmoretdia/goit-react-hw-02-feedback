b87e661d87cbc97f0273a41470cf988a4cbd57bc
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2023-12-03 21:31:35 +0100

    step 4

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Notification from '../Notification';
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
   return (
     <div>
       <h2>Statistics</h2>
-      <ul>
-        <li>Good: {good}</li>
-        <li>Neutral: {neutral}</li>
-        <li>Bad: {bad}</li>
-        <li>Total: {total}</li>
-        <li>Positive feedback: {positivePercentage}</li>
-      </ul>
+      {total > 0 ? (
+        <ul>
+          <li>Good: {good}</li>
+          <li>Neutral: {neutral}</li>
+          <li>Bad: {bad}</li>
+          <li>Total: {total}</li>
+          <li>Positive feedback: {positivePercentage}</li>
+        </ul>
+      ) : (
+        <Notification message="There is no feedback" />
+      )}
     </div>
   );
 };
