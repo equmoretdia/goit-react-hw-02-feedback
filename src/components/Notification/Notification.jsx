23b6918960b496ecaf45208d496a465f3cefbc0b
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2023-12-05 02:38:48 +0100

    Notification component is styled by CSS modules

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import css from './Notification.module.css';
 
 const Notification = ({ message }) => {
-  return <p>{message}</p>;
+  return (
+    <div className={css.wrapper}>
+      <p className={css.item}>{message}</p>
+    </div>
+  );
 };
 
 Notification.propTypes = {
