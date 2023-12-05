eacfd0e05bbb6c927401dde7a3a63dc718da6846
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2023-12-05 02:34:03 +0100

    Statistics component is styled by CSS modules

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Notification from '../Notification';
+import css from './Statistics.module.css';
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
   return (
     <div>
-      <h2>Statistics</h2>
+      <h2 className={css.title}>Statistics</h2>
       {total > 0 ? (
-        <ul>
-          <li>Good: {good}</li>
-          <li>Neutral: {neutral}</li>
-          <li>Bad: {bad}</li>
-          <li>Total: {total}</li>
-          <li>Positive feedback: {positivePercentage}</li>
+        <ul className={css.wrapper}>
+          <li className={css.item}>Good: {good}</li>
+          <li className={css.item}>Neutral: {neutral}</li>
+          <li className={css.item}>Bad: {bad}</li>
+          <li className={css.item}>Total: {total}</li>
+          <li className={css.item}>Positive feedback: {positivePercentage}</li>
         </ul>
       ) : (
         <Notification message="There is no feedback" />
